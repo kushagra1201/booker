0c86b9b1efcc5e094bfe215ce1a1787c65d6f3f7
kushagra1201 <imt_2019053@iiitm.ac.in>
2023-04-10 02:51:50 +0530

    added reservation and login components

---
+++ ui/src/components/reserve/Reserve.jsx
@@ -0,0 +1,79 @@
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./reserve.css";
+import useFetch from "../../hooks/useFetch.js";
+import { useContext, useState } from "react";
+import { SearchContext } from "../../context/SearchContext.js";
+
+const Reserve = ({ setOpen, hotelId }) => {
+  const [selectedRooms, setSelectedRooms] = useState([]);
+  const { data, loading, error } = useFetch(`hotels/room/${hotelId}`);
+  const { dates } = useContext(SearchContext);
+
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const date = new Date(start.getTime());
+
+    let list = [];
+
+    while (data <= end) {
+      list.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
+
+    return list;
+  };
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectedRooms(
+      checked
+        ? [...selectedRooms, value]
+        : selectedRooms.filter((item) => item !== value)
+    );
+  };
+
+  const handleClick = () => {};
+
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select Your Rooms: </span>
+        {data.map((item) => (
+          <div className="rItem">
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">
+                Max People: <b>{item.maxPeople}</b>
+              </div>
+              <div className="rPrice">{item.price}</div>
+            </div>
+            {item.roomNumbers.map((roomNumber) => (
+              <div className="room">
+                <label>{roomNumber.number}</label>
+                <input
+                  type="checkbox"
+                  value={roomNumber._id}
+                  onChange={handleSelect}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+        <button onClick={handleClick} className="rButton">
+          Reserve Now!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Reserve;
